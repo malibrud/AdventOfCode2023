b8487f3a3d9be12e7b1848a49b0c8dfa0c8031cb
malibrud <malibrud@gmail.com>
2023-12-26 00:12:19 -0500

    Day 15, part 1

---
+++ 15/part1.cpp
@@ -0,0 +1,54 @@
+// Solution to part 1 of https://adventofcode.com/2023/day/14
+// Mark Brudnak
+// Dec 22, 2023
+
+#include <cassert>
+
+import std;
+import std.compat;
+using namespace std;
+
+bool tryParseFileContents( char *fileName, string& outLine )
+{
+    try
+    {
+        // Open the file for reading
+        ifstream file( fileName );
+        if ( !file.is_open() ) return false;
+
+        getline( file, outLine );
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return false;
+    }
+    return true;
+}
+
+int main( int argc, char *argv[] )
+{
+    // Make sure we have an argument
+    if ( argc != 2 ) return 1;
+
+    string sequence;
+    if ( !tryParseFileContents( argv[1], sequence ) ) return 1;
+    int N = sequence.size();
+
+    int sum = 0;
+    int hash = 0;
+    for ( auto c : sequence )
+    {
+        if ( c == ',' )
+        {
+            sum += hash;
+            hash = 0;
+            continue;
+        }
+        hash += c;
+        hash *= 17;
+        hash &= 0xff;
+    }
+    sum += hash;
+    printf( "%d\n", sum );
+}
